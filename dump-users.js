7e3daf01a9fe272b483cbb9fce5c4b7817545074
Nguyễn Tuấn Anh <157350887+Tuan2Anh@users.noreply.github.com>
2026-04-22 21:26:30 +0700

    ...

--- dump-users.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const fs = require('fs');
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany();
-  fs.writeFileSync('users-dump.json', JSON.stringify(users, null, 2));
-}
-main().finally(() => prisma.$disconnect());
